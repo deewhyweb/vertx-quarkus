5adc90a9a1afb7e5a21d80b2c3fdb38be4ba669b
Philip Hayes <philip@deewhy.ie>
2022-08-24 16:50:52 -0400

    Migrated to quarkus

--- src/main/java/com/deewhy/starter/MainVerticle.java
+++ src/main/java/com/deewhy/starter/MainVerticle.java
@@ -1,31 +1,28 @@
 package com.deewhy.starter;
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Context;
-import io.vertx.core.Vertx;
+import io.quarkus.runtime.StartupEvent;
+import io.smallrye.mutiny.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.mutiny.core.Vertx;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 
 
-
+@ApplicationScoped
 public class MainVerticle extends AbstractVerticle {
 
     public static final String BASE_API_PATH = "/api/v1/services";
 
-    @Override
-    public void init(Vertx vertx, Context context) {
-        super.init(vertx, context);
-    }
 
-    @Override
-    public void start() {
-        Router router = Router.router(vertx);
+    public void init(@Observes StartupEvent e, Vertx vertx, MainVerticle verticle, Router router) {
+
         router.route().handler(BodyHandler.create());
         registerRoutes(router);
-        vertx.createHttpServer()
-                .requestHandler(router)
-                .listen(8080);
+        vertx.deployVerticle(verticle).await().indefinitely();
     }
+
 
     private void registerRoutes(Router router) {
         router.route("/*").handler(StaticHandler.create());
@@ -33,12 +30,13 @@ public class MainVerticle extends AbstractVerticle {
 
     }
 
+
     private void registerGetRoute(Router router) {
         router.get(BASE_API_PATH)
                 .handler(req -> {
                     req.response()
         .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
+        .end("Hello from quarkus!");
 
                 });
     }
--- src/test/java/com/deewhy/starter/TestMainVerticle.java
+++ src/test/java/com/deewhy/starter/TestMainVerticle.java
@@ -1,22 +1,4 @@
 package com.deewhy.starter;
 
-import io.vertx.core.Vertx;
-import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-@ExtendWith(VertxExtension.class)
-public class TestMainVerticle {
-
-  @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
-  }
-
-  @Test
-  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
-    testContext.completeNow();
-  }
+public class TestMainVerticle {  
 }
